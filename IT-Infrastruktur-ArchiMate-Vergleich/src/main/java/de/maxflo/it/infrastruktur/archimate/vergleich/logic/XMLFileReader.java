3de3db1affed094c2766505bf86d0f0ebcb14bce
Max Haag <mhaag@hm.edu>
2016-04-09 13:52:56 +0200

    Zwei Vergleicher mit eingebaut, eigener und Library. Eigener noch nicht alle Funktionen, evtl. reicht uns die Library die scheint auf den ersten Blick ganz ok zu sein.

--- IT-Infrastruktur-ArchiMate-Vergleich/src/main/java/de/maxflo/it/infrastruktur/archimate/vergleich/logic/XMLFileReader.java
+++ IT-Infrastruktur-ArchiMate-Vergleich/src/main/java/de/maxflo/it/infrastruktur/archimate/vergleich/logic/XMLFileReader.java
@@ -1,11 +1,13 @@
 package de.maxflo.it.infrastruktur.archimate.vergleich.logic;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.namespace.QName;
@@ -19,6 +21,8 @@ import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
+import org.custommonkey.xmlunit.DetailedDiff;
+import org.custommonkey.xmlunit.XMLUnit;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
@@ -31,9 +35,19 @@ import org.xml.sax.SAXException;
  * @author fn/mh
  */
 public class XMLFileReader {
-           
-
 /*
+    
+    09.04.2016
+    Eigener Vergleicher
+    ------------------
+    Versuch, mal testweise beide "nebeneinander iterieren" und Änderungen rausschreiben
+    Zeilen mail und phone vertauscht^^Noch nicht alle möglichen Fälle abgedeckt!!
+    
+    XMLUnit Vergleicher
+    ------------------
+    Joa...scheint seine Arbeit gut zu machen kannst dir ja mal anschaun
+
+    
     08.04.2016
     SAX und StAX Parser, schauen welcher besser ist...
     
@@ -44,50 +58,233 @@ public class XMLFileReader {
     private static boolean logging = true;
     private static Document doc;
     private static int count = 0;
-    
-    
-    
+
     public static void main(String[] args) {
         //readFile_SAX();
-        readFile_StAX();
-        //iterateFileTree();
+        //readFile_StAX();
+        //readAndCompFilesSelfmade();
+        readAndCompFilesLibrary();
+        
+    }
+    
+    private static void readAndCompFilesLibrary() {
+
+        String fileOrg = "Archisurance_kurz.archimate";
+        String fileMod = "Archisurance_kurz_changed.archimate";
+
+        //Einzel Strings...nicht so optimal
+        String line1 = "";
+        String line2 = "";
+        try (
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileOrg));
+            BufferedReader bufferedReader2 = new BufferedReader(new FileReader(fileMod));) {
+
+            String oneL = "";
+            while ((oneL = bufferedReader.readLine()) != null) {
+                line1+=oneL;
+            }
+            while ((oneL = bufferedReader2.readLine()) != null) {
+                line2+=oneL;
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        XMLUnit.setIgnoreWhitespace(true);
+        XMLUnit.setIgnoreAttributeOrder(true);
+
+        DetailedDiff diff = null;
+        try {
+            diff = new DetailedDiff(XMLUnit.compareXML(line1, line2));
+        } catch (SAXException ex) {
+            Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        List<?> allDifferences = diff.getAllDifferences();
+        
+        print(diff.toString());
     }
     
     
-        private static void readFile_StAX() {
+    
+  private static void readAndCompFilesSelfmade() {
+
         try {
-            String filePath = "Archisurance.archimate_KURZ";
-
-
-            Reader fileReader = null;
+            String fileOrg = "Archisurance_kurz.archimate";
+            String fileMod = "Archisurance_kurz_changed.archimate";
+                
+            Reader readerO = null;
+            Reader readerM = null;
             try {
-                fileReader = new FileReader(filePath);
+                readerO = new FileReader(fileOrg);
+                readerM = new FileReader(fileMod);
             } catch (FileNotFoundException ex) {
                 Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
             }
 
-            XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-            XMLEventReader xmlEventReader = null;
+            XMLInputFactory xmlInputFactoryO = XMLInputFactory.newInstance();
+            XMLInputFactory xmlInputFactoryM = XMLInputFactory.newInstance();
+            
+            XMLEventReader xmlEventReaderO = null;
+            XMLEventReader xmlEventReaderM = null;
             try {
-                xmlEventReader = xmlInputFactory.createXMLEventReader(fileReader);
+                xmlEventReaderO = xmlInputFactoryO.createXMLEventReader(readerO);
+                xmlEventReaderM = xmlInputFactoryM.createXMLEventReader(readerM);
             } catch (XMLStreamException ex) {
                 Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
             }
 
 
-            while (xmlEventReader.hasNext()) {
-                
-                XMLEvent xmlEvent = null;
+            //Orginal File
+            while (xmlEventReaderO.hasNext()) {
+
+                XMLEvent xmlEventO = null;
                 try {
-                    xmlEvent = xmlEventReader.nextEvent();
+                    xmlEventO = xmlEventReaderO.nextEvent();
                 } catch (XMLStreamException ex) {
                     Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 
-                if (xmlEvent.isStartElement()) {
+                //Andres auch noch weitere Elemente?
+                XMLEvent xmlEventM = null;
+                if(xmlEventReaderM.hasNext()) {
+                    try {
+                        xmlEventM = xmlEventReaderM.nextEvent();
+                    } catch (XMLStreamException ex) {
+                        Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+      
+                
+                //Erstes
+                if (xmlEventO.isStartElement()) {
+                    
+                    //Org Startelement
+                    StartElement startElO = xmlEventO.asStartElement();
+                    
+                    //Modifiziertes File auch StartElement, prüfen
+                    StartElement startElM = null;
+                    if(xmlEventM.isStartElement()) {
+                        startElM = xmlEventM.asStartElement();
+
+                        QName nameO = startElO.getName();
+                        QName nameM = startElM.getName();
+                        
+                        //TODO: Alles prüfen...
+                       if(!nameO.equals(nameM)) {
+                           print("Änderung erkannt, orginal "+ nameO.toString());
+                           print("Änderung erkannt, mod "+ nameM.toString()) ;
+                        }
+  
+                    } else {
+                        //Sonst ModElement weiter, restliche Fälle noch abdecken
+                        if(xmlEventReaderM.hasNext()) {
+                            try {
+                                xmlEventM = xmlEventReaderM.nextEvent();
+                            } catch (XMLStreamException ex) {
+                                Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        } 
+                    }
+
+                    //Iteration Attribute (innerhalb Startelemente)
+                    Iterator iterO = startElO.getAttributes();
+                    Iterator iterM = startElM.getAttributes();
+                    while (iterO.hasNext()) {
+                        Attribute attributO = (Attribute) iterO.next();
+                        QName nameO = attributO.getName();
+                        String valueO = attributO.getValue();
+
+                        if(iterM.hasNext()) {
+                            Attribute attributM = (Attribute) iterM.next();
+                            QName nameM = attributM.getName();
+                            String valueM = attributM.getValue();
+                            
+                            //TODO: Alles prüfen...
+                            if(!valueM.equals(valueO)) {
+                                print("Änderung erkannt, orginal "+ valueO.toString());
+                                print("Änderung erkannt, mod "+ valueM.toString()) ;
+                            }
+                            
+                        } else {
+                            //Attribute fehlen in Mod an dieser Stelle...
+                        }
+                    }
+                }
+                
+                //End Elemente
+                if (xmlEventO.isEndElement()) {
+                    EndElement endElO = xmlEventO.asEndElement();
+                    QName nameEndO = endElO.getName();
+                    
+                    if(xmlEventM.isEndElement()) {
+                        EndElement endElM = xmlEventM.asEndElement();
+                        QName nameEndM = endElM.getName();
+                        
+                        //TODO: Alles prüfen...
+                        if(!nameEndM.equals(nameEndO)) {
+                           print("Änderung erkannt, orginal "+ nameEndO.toString());
+                           print("Änderung erkannt, mod "+ nameEndM.toString()) ;
+                        }
+                    }
+                }
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+        
+        
+        
+    private static void readFile_StAX() {
+        
+        
+        try {
+            String fileOrg = "Archisurance_kurz.archimate";
+            String fileMod = "Archisurance_kurz_changed.archimate";
+                
+            Reader readerO = null;
+            Reader readerM = null;
+            try {
+                readerO = new FileReader(fileOrg);
+                readerM = new FileReader(fileMod);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            XMLInputFactory xmlInputFactoryO = XMLInputFactory.newInstance();
+            XMLInputFactory xmlInputFactoryM = XMLInputFactory.newInstance();
+            
+            XMLEventReader xmlEventReaderO = null;
+            XMLEventReader xmlEventReaderM = null;
+            try {
+                xmlEventReaderO = xmlInputFactoryO.createXMLEventReader(readerO);
+                xmlEventReaderM = xmlInputFactoryO.createXMLEventReader(readerM);
+            } catch (XMLStreamException ex) {
+                Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+
+            //Orginal File
+            while (xmlEventReaderO.hasNext()) {
+
+                XMLEvent xmlEventO = null;
+                try {
+                    xmlEventO = xmlEventReaderO.nextEvent();
+                } catch (XMLStreamException ex) {
+                    Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                
+                if (xmlEventO.isStartElement()) {
                     
                     //Get event as start element. 
-                    StartElement startElement = xmlEvent.asStartElement();
+                    StartElement startElement = xmlEventO.asStartElement();
                     print("Start Element: " + startElement.getName());
 
                     //Iterate and process attributes. 
@@ -102,18 +299,21 @@ public class XMLFileReader {
                 }
                 
                 //Check if event is the end element. 
-                if (xmlEvent.isEndElement()) {
+                if (xmlEventO.isEndElement()) {
                 //Get event as end element. 
-                    EndElement endElement = xmlEvent.asEndElement();
+                    EndElement endElement = xmlEventO.asEndElement();
                     print("End Element: " + endElement.getName());
                 }
+                
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    
+    
 
-
+    
     
     
     private static void readFile_SAX() {
@@ -138,11 +338,13 @@ public class XMLFileReader {
             Logger.getLogger(XMLFileReader.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        //Normalisieren
+        
+                //Normalisieren
         doc.getDocumentElement().normalize();
         
         Element root = doc.getDocumentElement();
         print(root.getNodeName());
+
 
         //Ebenen innerhalb 
         //archimate:model
@@ -188,7 +390,7 @@ public class XMLFileReader {
         if(logging) {
             Logger.getLogger(XMLFileReader.class.getName()).log(Level.INFO, toPrint);
         } else {
-            System.out.print(toPrint); 
+            System.out.println(toPrint); 
         }
     }
 }
